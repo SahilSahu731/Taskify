d462eb8af8776521e7d2aea127e1309d47f1b1e5
Sahil Sahu <sahilsahu731709@gmail.com>
2025-07-13 11:19:47 +0530

    added ui library shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,52 +1,103 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Open Sans', 'sans-serif'],
-        dancing: ['Dancing Script', 'cursive'],
-      },
-      colors: {
-        'blue-700': '#1D4ED8',
-        'blue-800': '#1E40AF',
-        'red-600': '#DC2626',
-        'red-700': '#B91C1C',
-        'white': '#FFFFFF',
-        'gray-800': '#1F2937',
-        'zinc-900': '#111827',
-        'slate-800': '#374151',
-      },
-      boxShadow: {
-        'lg': '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      },
-      transitionProperty: {
-        'width': 'width',
-        'height': 'height',
-        'background-color': 'background-color',
-        'transform': 'transform',
-      },
-      transitionDuration: {
-        '200': '200ms',
-        '500': '500ms',
-      },
-      scale: {
-        '105': '1.05',
-        '110': '1.10',
-      },
-      dropShadow: {
-        'lg': '0 10px 15px rgba(0, 0, 0, 0.2)',
-      },
-      opacity: {
-        '90': '0.9',
-        '80': '0.8',
-      },  
-
-    },
+  	extend: {
+  		fontFamily: {
+  			sans: [
+  				'Open Sans',
+  				'sans-serif'
+  			],
+  			dancing: [
+  				'Dancing Script',
+  				'cursive'
+  			]
+  		},
+  		colors: {
+  			'blue-700': '#1D4ED8',
+  			'blue-800': '#1E40AF',
+  			'red-600': '#DC2626',
+  			'red-700': '#B91C1C',
+  			white: '#FFFFFF',
+  			'gray-800': '#1F2937',
+  			'zinc-900': '#111827',
+  			'slate-800': '#374151',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		boxShadow: {
+  			lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)'
+  		},
+  		transitionProperty: {
+  			width: 'width',
+  			height: 'height',
+  			'background-color': 'background-color',
+  			transform: 'transform'
+  		},
+  		transitionDuration: {
+  			'200': '200ms',
+  			'500': '500ms'
+  		},
+  		scale: {
+  			'105': '1.05',
+  			'110': '1.10'
+  		},
+  		dropShadow: {
+  			lg: '0 10px 15px rgba(0, 0, 0, 0.2)'
+  		},
+  		opacity: {
+  			'80': '0.8',
+  			'90': '0.9'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
